46e8748c20ac1f511ea4d789efa7ced088cca9f6
pragya617 <79211225+pragya617@users.noreply.github.com>
2021-02-25 23:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ some random file
 print("this is the third line")
 name = 'pragya'
 print(name)
-
 ---------------------------------------------
-
 print("code addedby 2nd developer")
 print("Line added by 1st developer")
 print("line added by dev branch")
